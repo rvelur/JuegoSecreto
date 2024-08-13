80b2d3801f4b7f488fc130b08ac3ad0982b51e26
rvelur <rvelasquez579@gmail.com>
2024-08-13 00:45:25 -0400

    Versión final juego del número secreto

---
+++ app.js
@@ -0,0 +1,73 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+function asignarTextoElemento (elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    if(numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else {
+        //EL USUARIO NO ACERTÓ
+        if(numeroDeUsuario > numeroSecreto){
+            asignarTextoElemento('p', 'El número secreto es menor');
+        }else {
+            asignarTextoElemento('p', 'El número secreto es mayor');
+        }
+        intentos++; 
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value= '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+
+    //SI YA SORTEAMOS TODOS LOS NUMEROS
+
+    if(listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p', 'Ya se sortearon todos los números posibles');
+    } else {
+        //SI EL NUMERO GENERADO ESTA INCLUIDO EN LA LISTA    
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        }else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function condicionesIniciales () {
+    asignarTextoElemento('h1', 'Juego del número secreto');
+    asignarTextoElemento('p', `Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego () {
+    //LIMPIAR CAJA
+    limpiarCaja();
+    //INDICAR MENSAJE DE INTERVALO DE NÚMEROS
+    //GENERAR EL NUMERO ALEATORIO
+    //INICIALIZAR EL NUMERO DE INTENTOS
+    condicionesIniciales();
+    //DESHABILITAR EL BOTON DE NUEVO JUEGO
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+}
+
+condicionesIniciales ();
